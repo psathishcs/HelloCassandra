f29e0d867c04351903c8aa151b0ccb621aa0ade6
DESKTOP-7D1NSHG\Sathish <p.sathish.cs@gmail.com>
2017-01-15 14:02:21 -0600

    swagger2 fix and setting the base path of project as /cassandra/api

--- src/main/java/org/hello/cassandra/spring/Application.java
+++ src/main/java/org/hello/cassandra/spring/Application.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,7 +24,7 @@ public class Application {
 	    return new Docket(DocumentationType.SWAGGER_2)          
 	      .select()
 	      .apis(RequestHandlerSelectors.basePackage("org.hello.cassandra.spring.controller"))
-	      .paths(PathSelectors.ant("/*"))
+	      .paths(PathSelectors.ant("/cassandra/api/*"))
 	      .build()
 	      .apiInfo(apiInfo());
 	}
@@ -34,7 +35,7 @@ public class Application {
 	      "Some custom description of API.",
 	      "API TOS",
 	      "Terms of service",
-	      "myeaddress@company.com",
+	      new Contact("Sathish Kumar Parthasarathy", "https://github.com/psathishcs", "p.sathish.cs@gmail.com"),
 	      "License of API",
 	      "API license URL");
 	    return apiInfo;
